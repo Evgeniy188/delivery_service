3cff6ff5c3c7b18b4b6dba4c48daa603dd9a91f9
Evgeniy Lapshin <trytostopme2010@yandex.ru>
2024-01-29 20:10:45 +0300

    исправлено решение методлом 2х указателей

--- delivery_service.py
+++ delivery_service.py
@@ -1,31 +1,25 @@
-import sys
-
-
-def main():
-    seq = list(map(int, sys.stdin.readline().rstrip().split(' ')))
-    limit = int(sys.stdin.readline().rstrip())
-    print(delivery_service(seq, limit))
-
-
-def delivery_service(seq: list, limit: int) -> int:
+# 106143761
+def delivery_service(weight_robot_list: list, limit: int) -> int:
     """Функция для определения минимального количества транспортных платформ,
     необходимое для перевозки всех роботов, описанных в массиве"""
-    seq.sort()
+    weight_robot_list_sort = sorted(weight_robot_list)
     count_platform = 0
-    right_pointer = len(seq) - 1
-    while right_pointer > 0:
-        current_sum = seq[0] + seq[right_pointer]
+    left_pointer = 0
+    right_pointer = len(weight_robot_list_sort) - 1
+    while left_pointer <= right_pointer:
+        current_sum = (weight_robot_list_sort[left_pointer] +
+                       weight_robot_list_sort[right_pointer])
         if current_sum <= limit:
-            seq.pop(right_pointer)
-            seq.pop(0)
+            left_pointer += 1
+            right_pointer -= 1
             count_platform += 1
-            right_pointer = len(seq) - 1
-            continue
         else:
             right_pointer -= 1
-    count_platform += len(seq)
+            count_platform += 1
     return count_platform
 
 
 if __name__ == '__main__':
-    main()
+    weight_robot_list = [int(x) for x in input().split(' ')]
+    limit = int(input())
+    print(delivery_service(weight_robot_list, limit))
